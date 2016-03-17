e87510b692d4ab355d4d315832a60441cd7d5b61
tutopenkin <tutopenkin@gmail.com>
2016-03-17 13:37:46 +0000

    Tried to fix problem with restarting measurements

--- measurement.sh
+++ measurement.sh
@@ -18,24 +18,27 @@ GPSERR=0
 online=1
 
 # Functions
+
+function logger {
+	echo "[$(date)] $1" >> $LOGFILE
+	echo "[$(date)] $1"
+}
+
 function quitScreens {
 	if screen -list | grep -q "gps"; then
-		echo "Shutting down the gps"
-		echo "Shutting down the gps" >> $LOGFILE
+		logger "Shutting down the gps"
 
 		screen -S gps -X quit
 	fi
 
 	if screen -list | grep -q "imu"; then
-		echo "Shutting down the imu" >> $LOGFILE
-		echo "Shutting down the imu"
+		logger "Shutting down the imu"
 
 		screen -S imu -X quit
 	fi
 
 	if screen -list | grep -q "bag"; then
-		echo "Shutting down bag" >> $LOGFILE
-		echo "Shutting down bag"
+		logger "Shutting down bag"
 		#The bag needs to exit gracefully
 		screen -S bag -X stuff $'\003'
 		sleep 3
@@ -46,8 +49,7 @@ function quitScreens {
 function check3G {
 	if [ "$online" -ne 0 ]; then
 		if lsusb | grep -q 12d1:1003; then
-			echo "Connecting 3G"
-			echo "Connecting 3G" >> $LOGFILE
+			logger "Connecting 3G"
 			# Connect to 3G with Huawei E160, saunalahti
 			sudo sakis3g connect OTHER="USBMODEM" USBMODEM="12d1:1003" APN="internet.saunalahti"
 		fi
@@ -83,7 +85,8 @@ while true; do
 
 	check3G
 
-	nc -w 3 -z 8.8.8.8 53  >/dev/null 2>&1
+	# enough timeout for 3g
+	nc -w 10 -z 8.8.8.8 53  >/dev/null 2>&1
 #	if [ -f "online" ]; then
 #		online=0
 #	else
@@ -101,8 +104,7 @@ while true; do
 		if [ "$STOPPED" -ne 0 ]; then
 
 			#Uploading the data
-			echo "Waiting for everything to shut down" >> $LOGFILE
-			echo "Waiting for everything to shut down"
+			logger "Waiting for everything to shut down"
 			while screen -list | grep "imu\|gps\|bag"; do
 				sleep 2
 			done
@@ -114,11 +116,10 @@ while true; do
 
 		if [ "$UPLOADED" -ne 0 ]; then
 			if ! ps -p $GRIVEPID > /dev/null 2>&1; then
-				echo "Uploading the data" >> $LOGFILE
-				echo "Uploading the data"
+				logger "Uploading the data"
 				#screen -r grive -X stuff $'\ngrive\n'
 				# upload in subshell, wait for finish (should we?)
-				(cd $DATADIR; grive) &
+				(cd $DATADIR; grive) & >> $LOGFILE
 				GRIVEPID=$!
 			fi
 			UPLOADED=0
@@ -128,15 +129,13 @@ while true; do
 		UPLOADED=1
 
 #		if [ "$GRIVEPID" -ne -1 ]; then
-#			echo "Stopping GRIVE, measuring, $GRIVEPID"
-#			echo "Stopping GRIVE, measuring, $GRIVEPID" >> $LOGFILE
+#			logger "Stopping GRIVE, measuring, $GRIVEPID"
 #			kill -- -"$GRIVEPID"
 #			GRIVEPID=-1
 #		fi
 
 		if [ "$GPSERR" -gt 10 ]; then
-			echo "GPS-errors more than 10, restarting"
-			echo "GPS-errors more than 10, restarting" >> $LOGFILE
+			logger "GPS-errors more than 10, restarting"
 			GPSERR=0
 			quitScreens
 		fi
@@ -144,8 +143,7 @@ while true; do
 		#NOTE! The gps is a launch file which will take care of starting the ros core
 		#If this is not used, separate core process must be launched
 		if ! screen -list | grep -q "gps"; then
-			echo "Offline: Starting GPS and ROSCORE"
-			echo "Offline: Starting GPS and ROSCORE" >> $LOGFILE
+			logger "Offline: Starting GPS and ROSCORE"
 			screen -dmS gps
 			screen -r gps -X stuff $'\nrm '$GPSTEMPFILE$'\nroslaunch ublox_gps ublox_gps.launch 2> '$GPSTEMPFILE$'\n'
 
@@ -154,16 +152,13 @@ while true; do
 
 	                # if file exists and not empty
 	                if [ -s $GPSTEMPFILE ]; then
-				echo "Error on starting gps"
-				cat $GPSTEMPFILE
-				echo "Error on starting gps" >> $LOGFILE
-				cat $GPSTEMPFILE >> $LOGFILE
+				logger "Error on starting gps"
+				logger $GPSTEMPFILE
 	                        screen -S gps -X quit
 	                        # start over
 	                        continue
 	                elif [ ! -f $GPSTEMPFILE ]; then
-				echo "GPSlog not found"
-				echo "GPSlog not found" >> $LOGFILE
+				logger "GPSlog not found"
 				screen -S gps -X quit
                                 # start over
 				continue
@@ -175,15 +170,13 @@ while true; do
 		if screen -list | grep -q "gps"; then
 
 			if [ "$IMUERR" -gt 10 ]; then
-				echo "IMU-errors more than 10, restaring IMU"
-				echo "IMU-errors more than 10, restaring IMU" >> $LOGFILE
+				logger "IMU-errors more than 10, restaring IMU"
 				IMUERR=0
 				screen -S imu -X quit
 			fi
 
 			if ! screen -list | grep -q "imu"; then
-				echo "Offline: Starting IMU"
-				echo "Offline: Starting IMU" >> $LOGFILE
+				logger "Offline: Starting IMU"
 				screen -dmS imu
 				# /dev/serial/by-id/usb-Xsens_Xsens_COM_port_00342762-if00
 				#screen -r imu -X stuff $'\nrosrun xsens_driver mtnode_new.py _device:=/dev/serial/by-id/usb-Xsens_Xsens_COM_port_00340764-if00\n'
@@ -191,25 +184,29 @@ while true; do
 			fi
 
 			if ! screen -list | grep -q "bag"; then
-				echo "Offline: Starting RECORDING"
-				echo "Offline: Starting RECORDING" >> $LOGFILE
+				logger "Offline: Starting RECORDING"
 				screen -dmS bag
 				screen -r bag -X stuff $'\nrosbag record -a\n'
 			fi
 		else
-			echo "GPS wasn't running!"
-			echo "GPS wasn't running!" >> $LOGFILE
+			logger "GPS wasn't running!"
 		fi
 
 		STOPPED=1
 
 		# Check we have wanted topics (so working)
+		# TODO: might fail for some reason? Causing recording to restart...
+		# Might be fixed with zeroing of errors
 		if ! rostopic list | grep -q "/imu/data"; then
 			((IMUERR++))
+		else
+			IMUERR=0
 		fi
 
 		if ! rostopic list | grep -q "/gps/fix"; then
 			((GPSERR++))
+		else
+			GPSERR=0
 		fi
 
 	fi
