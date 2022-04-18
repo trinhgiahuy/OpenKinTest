9f38af5d1363bef5644ad9538062e855af0e915e
trinhgiahuy <65078173+trinhgiahuy@users.noreply.github.com>
2022-04-18 13:13:16 +0300

    Now it can run

--- m5stickC/imu-test.ino
+++ m5stickC/imu-test.ino
@@ -18,11 +18,14 @@ float yaw = 0.0F;
 */
 void setup(){
   M5.begin();             //Init M5StickC plus
-  M5.Lcd.setCusor(80, 15);
+  M5.IMU.Init();
+  M5.Lcd.setRotation(3);
+  M5.Lcd.setCursor(80, 15);
   M5.Lcd.println("IMU data test");
-  M5.Lcd.setCusor(30, 30);
-  M5.Lcd.println(" X       Y       Z");
-  M5.Lcd.setCusor(" Pitch   Roll    Yaw");
+  M5.Lcd.setCursor(30, 30);
+  M5.Lcd.println("  X       Y       Z");
+  M5.Lcd.setCursor(30,70);
+  M5.Lcd.println("  Pitch   Roll    Yaw");
 }
 
 void loop(){
@@ -32,8 +35,20 @@ void loop(){
   M5.IMU.getGyroData(&gyroX, &gyroY, &gyroZ);
   M5.IMU.getAccelData(&accX, &accY, &accZ);
   M5.IMU.getAhrsData(&pitch, &roll, &yaw);
-  M5.IMU.getTempData(&temp);
-  
+  M5.IMU.getTempData(&tmp);
+  M5.Lcd.setCursor(30, 40);
+  M5.Lcd.printf("%6.2f  %6.2f  %6.2f      ", gyroX, gyroY, gyroZ);
+  M5.Lcd.setCursor(170, 40);
+  M5.Lcd.print("o/s");
+  M5.Lcd.setCursor(30, 50);
+  M5.Lcd.printf(" %5.2f   %5.2f   %5.2f   ", accX, accY, accZ);
+  M5.Lcd.setCursor(170, 50);
+  M5.Lcd.print("G");
+  M5.Lcd.setCursor(30, 80);
+  M5.Lcd.printf(" %5.2f   %5.2f   %5.2f   ", pitch, roll, yaw);
+
+  M5.Lcd.setCursor(30, 95);
+  M5.Lcd.printf("Temperature : %.2f C", tmp);
   delay(1000);         //Delay 1s
   
 }
