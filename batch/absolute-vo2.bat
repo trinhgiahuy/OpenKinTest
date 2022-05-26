5299c1e7f8d20a3553297f02c629ceb36acd282b
trinhgiahuy <65078173+trinhgiahuy@users.noreply.github.com>
2022-05-26 14:01:13 +0700

    Update absolute-vo2.bat

--- batch/absolute-vo2.bat
+++ batch/absolute-vo2.bat
@@ -4,7 +4,7 @@ REM (
 for /f "usebackq tokens=1 delims=, skip=8" %%i in (D:\a.csv) do (
 ::setlocal enabledelayedexpansion
 call :PROCESS %%i
-)>> a.log
+)
 REM )
 pause
 ::endlocal
@@ -27,63 +27,63 @@ call :strlen result "%1"
 
 setlocal enabledelayedexpansion
 set tmp_data=%~1
-echo before loop 3 !tmp_data!
-echo before loop 6 !tmp_data!
+REM echo before loop 3 !tmp_data!
+REM echo before loop 6 !tmp_data!
 ::result=3: NO HOUR (31:30)
 ::result=6: HAVE HOUR (1:25:45)
-if %result%==3 (
-echo after loop 3 !tmp_data!
+if !result!==3 (
+REM echo after loop 3 !tmp_data!
 REM call :ECHO3 %tmp_data%
 ::echo %result% loop result = 3
 set /A tmp_mins = 1!tmp_data:~0,2! - 100
-set /A tmp_secs = !tmp_data:~3,2!
+set /A tmp_secs = 1!tmp_data:~3,2! - 100
 ::echo !tmp_mins! !tmp_secs! 
-set new_hours = !clk_hour!
-REM if !tmp_mins!==08 (
-REM echo 08
-REM set tmp_mins=8
-REM )
-
-REM if !tmp_mins!==09 (
-REM echo 09
-REM set tmp_mins=9
-REM )
+set /A new_hours = !clk_hour! + 0
 set /A new_mins = !clk_mins! + !tmp_mins!
 set /A new_secs = !clk_secs! + !tmp_secs!
 
-echo new3 !new_hours! !new_mins! !new_secs! 
+REM echo new3 !new_hours! !new_mins! !new_secs! 
 )
 
-if %result%==6 (
-echo after loop 6 !tmp_data! 
+if !result!==6 (
+REM echo after loop 6 !tmp_data! 
 ::echo %result% loop result = 6
 set tmp_hours=!tmp_data:~0,1!
 set /A tmp_mins=1!tmp_data:~2,2!-100
 set /A tmp_secs=1!tmp_data:~5,2!-100
-echo _____new6 !tmp_hours! !tmp_mins! !tmp_secs!
+REM echo _____new6 !tmp_hours! !tmp_mins! !tmp_secs!
 set /A new_hours=!clk_hour!+!tmp_hours!
 set /A new_mins=!clk_mins!+!tmp_mins!
 set /A new_secs=!clk_secs!+!tmp_secs!
-echo new6 !tmp_hours! !tmp_mins! !tmp_secs!
+REM echo new6 !tmp_hours! !tmp_mins! !tmp_secs!
 )
 
 ::Check if secs and hours are larger than 60
 if !new_secs! geq 60 (
-echo ---------set60secs
-echo ---beforeadd !new_mins! !new_secs!
+REM echo ---------set60secs
+REM echo ---beforeadd !new_mins! !new_secs!
 set /A new_mins = !new_mins! + 1
 set /A new_secs = !new_secs! - 60
-echo ---afteradd !new_mins! !new_secs!
+REM echo ---afteradd !new_mins! !new_secs!
 )
 
 if !new_mins! geq 60 (
-echo ---------set60mins
-echo ---beforeadd !new_hours! !new_mins! !new_secs!
+REM echo ---------set60mins
+REM echo ---beforeadd !new_hours! !new_mins! !new_secs!
 set /A new_hours = !new_hours!+1
 set /A new_mins = !new_mins!-60
-echo ---afteradd !new_hours! !new_mins! !new_secs!
+REM echo ---afteradd !new_hours! !new_mins! !new_secs!
 )
+REM if !result!==3 (
+REM echo !new_mins!:!new_secs!
+REM )
+REM if !result!==6 (
+REM echo !new_hours!:!new_mins!:!new_secs!
+REM )
 
+echo !new_hours!:!new_mins!:!new_secs!
+REM call :strlen result2 "!new_secs!"
+REM echo "!new_secs!" %result2%
 
 REM :ECHO3
 REM ::echo callecho3 %1
