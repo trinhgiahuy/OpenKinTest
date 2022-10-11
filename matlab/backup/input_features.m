4e0ef12472f1d65dc4bf512b6a44da9378b557a8
trinhgiahuy <65078173+trinhgiahuy@users.noreply.github.com>
2022-10-11 19:07:13 +0300

    Backup input_features script

---
+++ matlab/backup/input_features.m
@@ -0,0 +1,55 @@
+function [] = input_features(subjectId,sorted)
+% Multiple features
+% Start from the step 
+%clear inputFeatures
+%clear oxygen
+%clear targetFeatures
+
+oxygen = csvread(['O',num2str(subjectId),'_output_smooth.csv']);
+load (['velSeg_O',num2str(subjectId),'_out.mat']);
+step0 = 50;
+stepEnd = length(speed);
+examples = stepEnd - step0;
+numTimeSteps = 50;
+inputFeatures = cell(1,examples);
+targetFeatures = zeros(1,examples,1);
+
+%example 1598
+%numTimeSteps 100
+%k:1-1498
+for k=1:(examples-numTimeSteps)
+      
+   time(k) = steps(k)/400;
+   sequence = (step0+k):(step0+k+numTimeSteps-1);
+   inputFeatures{k}(1,:) =  speed(sequence);
+   inputFeatures{k}(2,:) =  speedChange(sequence);
+   inputFeatures{k}(3,:) =  stepDuration(sequence);
+   inputFeatures{k}(4,:) =  vertOscillation_dist_amp(sequence);
+   
+   %disp('j');
+   j = floor(time(k)/5)+1;
+   targetFeatures(k) = oxygen(j);
+  
+end
+
+%length(inputFeatures(1,:))
+
+[idxTrain,idxTest] = trainingPartitions(examples-numTimeSteps,[0.8 0.2]);
+
+if sorted
+    idxTrain=sort(idxTrain);
+    idxTest=sort(idxTest);
+
+XTrain = inputFeatures(idxTrain);
+%XValidation = inputFeatures(idxValidation);
+
+XTest = inputFeatures(idxTest);
+
+TTrain = targetFeatures(idxTrain);
+%TValidation = targetFeatures(idxValidation);
+TTest = targetFeatures(idxTest);
+
+%fileName=['inputFeaturesss',num2str(subjectId),'_sort'];
+%save(fileName,'XTrain','XTest','TTrain','TTest')
+save inputFeatures3_NOOsort.mat XTrain XTest TTrain TTest
+end
